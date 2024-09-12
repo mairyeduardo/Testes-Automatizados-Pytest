ed8f8462d06ace49b04e5bd0b3e222e440b4966d
mairyeduardo <mairyeduardo@gmail.com>
2024-09-12 16:17:16 -0300

    mensagens uteis para o contexto dos testes

--- Pytest/3.PrimeirosTestes/TestesEstuturais/test_estruturais.py
+++ Pytest/3.PrimeirosTestes/TestesEstuturais/test_estruturais.py
@@ -2,4 +2,6 @@
 def test_listas_iguais():
     lista_esperada = [1,2,3,4,5]
     lista_resultado = [1,2,3,4,5]
+    # Pode ser colocado uma mensagem no assert caso o resultado falhe,
+    # serve para analises
     assert lista_resultado == lista_esperada, "As listas não são iguais."
