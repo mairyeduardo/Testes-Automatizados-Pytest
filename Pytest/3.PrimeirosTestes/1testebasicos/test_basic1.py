4c9c5339a51eddfe248dbb4eacb03e4b27e7e7a0
mairyeduardo <mairyeduardo@gmail.com>
2024-09-12 15:25:02 -0300

    realizando alterações nos testes

--- Pytest/3.PrimeirosTestes/1testebasicos/test_basic1.py
+++ Pytest/3.PrimeirosTestes/1testebasicos/test_basic1.py
@@ -1,6 +1,13 @@
+# Função que retorna ture se o número for positivo ou seja > 0
+
 def is_positive(numero):
     return numero > 0
 
+
 def test_eh_positivo():
     assert is_positive(5) is True
-    assert is_positive(-5) is False 
+    assert is_positive(-5) is False
+
+# exemplo de teste quebrando pois queremos garantir que retorne um False,
+# porem 2 é um numero positivo, retornando True
+# assert is_positive(2) is False
