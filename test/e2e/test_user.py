e5c65063ec0fa7900577da617ee0931b5b4f0b32
mairyeduardo <mairyeduardo@gmail.com>
2025-05-20 17:26:48 -0300

    criacao da branch, adicionando validacoes no teste de criacao de usuario e criando teste de get_user_by_id

--- test/e2e/test_user.py
+++ test/e2e/test_user.py
@@ -4,7 +4,20 @@ from test.utils.requestToTest.request_to_test import request_to_test
 
 class TestUser:
     URL = "https://api-qas-trainings.glitch.me/users"
+    request_to_specific_url = f"{URL}/3"
 
     def test_create_user(self, url=URL):
         response = request_to_test("POST", url, PAYLOAD_TO_CREATE_USER)
+        data = response.json()
         assert response.status_code == 201
+        assert data["name"] == PAYLOAD_TO_CREATE_USER["name"]
+        assert data["email"] == PAYLOAD_TO_CREATE_USER["email"]
+
+    def test_get_user_by_id(self, url=request_to_specific_url):
+        response = request_to_test("GET", url)
+        data = response.json()
+        assert response.status_code == 200
+        assert data["id"] == 3
+        assert data["name"] == PAYLOAD_TO_CREATE_USER["name"]
+        assert data["email"] == PAYLOAD_TO_CREATE_USER["email"]
+
